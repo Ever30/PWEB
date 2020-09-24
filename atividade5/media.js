8fa7df8a49e345eb5c98271b41d928ad449b90d1
Everdrone2 <40778958+Everdrone2@users.noreply.github.com>
2020-09-23 23:14:02 -0300

    Add files via upload

---
+++ atividade5/media.js
@@ -0,0 +1,20 @@
+
+var nome, n1, n2, n3, media;
+
+	
+nome= prompt ("Informe o nome");
+
+n1= prompt ("Informe a primeira nota: ") ;
+
+
+n2= prompt ("Informe a segunda nota: ") ;
+ 
+
+n3= prompt ("Informe a terceira nota: ") ; 
+
+media=((parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3);
+
+alert ((nome) + "Sua média é:  " + (media));
+
+
+
---
+++ atividade5/operacoes.js
@@ -0,0 +1,29 @@
+var numero1, numero2,soma, subtracao, produto, divisao, resto;
+
+numero1 = prompt("Informe o primeiro número");
+
+numero2 = prompt("Informe o segundo número");
+
+soma= parseFloat(soma);
+soma = (parseFloat(numero1) + parseFloat(numero2));
+
+subtracao = (parseFloat(subtracao));
+subtracao = (parseFloat(numero1) - parseFloat(numero2));
+
+produto =(parseFloat(produto));
+produto =(parseFloat(numero1) * parseFloat(numero2));
+
+divisao =(parseFloat(divisao));
+divisao =(parseFloat(numero1)/parseFloat(numero2));
+
+resto = parseFloat(resto);
+resto = (parseFloat(numero1)% parseFloat(numero2));
+
+alert("Soma: " +(soma)); 
+alert("subtracao: " + (subtracao));
+alert("Produto: " + (produto));
+alert("Divisão: " + (divisao));
+alert("Resto: " + (resto));
+
+
+
