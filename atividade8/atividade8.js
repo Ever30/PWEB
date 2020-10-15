e2c39151a1efbe6e712ce320daa4a245bcab31ce
Everdrone2 <40778958+Everdrone2@users.noreply.github.com>
2020-10-14 22:47:53 -0300

    Add files via upload

---
+++ atividade8/atividade8.js
@@ -0,0 +1,14 @@
+  
+var  Aluno1  =  {
+  ra : '0030481723002' ,
+  nome : 'Everton'
+}
+alert ( "RA:"  +  Aluno1 . ra  +  "\ nNome:"  +  Aluno1 . nome ) ;
+
+Aluno1 . ra  =  '0030481723002' ;
+Aluno1 . nome  =  'Everton' ;
+alert ( "RA:"  +  Aluno1 . ra  +  "\ nNome:"  +  Aluno1 . nome ) ;
+
+Aluno1 [ 'ra' ]  =  '0030481723002' ;
+Aluno1 [ 'nome' ]  =  'Everton' ;
+alert ( "RA:"  +  Aluno1 . ra  +  "\ nNome:"  +  Aluno1 . nome ) ;
